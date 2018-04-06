f0dcc2d318d427b62014280c9bc68345e1fdb7a7
Yoni Cohen <yoniwcohen@gmail.com>
2018-04-06 15:13:19 +0300

    Added dist dir

--- src/app.ts
+++ src/app.ts
@@ -30,7 +30,6 @@ app.get('/tree/:packageName/:packageVersion', (req: Request, res: Response, next
     });
 });
 
-// no stacktraces leaked to user
 app.use(function (err: any, req: Request, res: Response, next: any) {
     res.status(err.status || 500).send(err);
 });
